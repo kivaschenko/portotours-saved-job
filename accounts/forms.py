9c3b8aeff13202606bb1f7ffe89ba7be0144cd3d
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-01-27 13:51:38 +0200

    remove exciting from form

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,9 +5,6 @@ from accounts.models import User
 
 
 class CustomSignupForm(UserCreationForm):
-    pass
-    # email = forms.EmailField(widget=forms.widgets.EmailInput())
-
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email',)
