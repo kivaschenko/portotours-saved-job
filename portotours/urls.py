a88f4cb18457f32d1ffee4f47c3e1950f027674a
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-01-30 15:37:15 +0200

    added test page destination detail for front end develope

--- portotours/urls.py
+++ portotours/urls.py
@@ -1,25 +1,10 @@
-"""
-URL configuration for portotours project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 from accounts import views as accounts_views
+from products import views as products_views
 
 # HOME & ACCOUNTS
 urlpatterns = [
@@ -36,6 +21,12 @@ urlpatterns = [
     # accounts/password_reset/done/ [name='password_reset_done']
     # accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
     # accounts/reset/done/ [name='password_reset_complete']
+]
+
+# DESTINATIONS
+urlpatterns += [
+    path('products/portugal/destinations/some-test-slug-for-current-destionation-detail-view/',
+         products_views.DestinationDetailView.as_view(), name="destination-detail"),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
