c52c55b6b6334f959201c3f45a73a5e56c451dfc
Koshliak Pasha <p.koshliak@avic.ua>
2024-02-02 10:11:08 +0200

    added footer styles, finished destination page layout

--- products/views.py
+++ products/views.py
@@ -19,7 +19,7 @@ class DestinationListView(ListView):
 
 class DestinationDetailView(DetailView):
     model = Destination
-    template_name = 'destinations/destination.html'  # rename to 'destination_detail.html'
+    template_name = 'destinations/destination_detail.html'  # rename to 'destination_detail.html'
 
 
 class DestinationLanguageListView(ListView):
