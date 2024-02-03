05c78cd3712960ce29de581e7e47b5f21f8b4955
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-02-03 20:36:03 +0200

    fixed issues, try run again

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Pull the latest Docker image
-docker pull kivaschenko/portotours:latest
+sudo docker pull kivaschenko/portotours:latest
 
 # Stop and remove the existing Docker container
-docker stop django-portotours || true
-docker rm django-portotours || true
+sudo docker stop django-portotours || true
+sudo docker rm django-portotours || true
 
 # Run the Docker container with the new image
-docker run -d --name django-portotours -p 8000:8000 kivaschenko/portotours:latest
+sudo docker run -d --name django-portotours -p 8000:8000 kivaschenko/portotours:latest
