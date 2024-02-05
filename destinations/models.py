6688e382fd3ada64a079e630433c3f8bf6a14dfd
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-02-05 19:03:41 +0200

    fixed Attraction, Destiantion: fields added according figma sample

--- destinations/models.py
+++ destinations/models.py
@@ -77,7 +77,12 @@ class Destination(models.Model):
 
     be_interested_title = models.CharField(max_length=120, help_text="max 120 characters", null=True, blank=True)
     be_interested_subtitle = models.CharField(max_length=120, help_text="max 120 characters", null=True, blank=True)
-
+    # Recommendations block
+    recommendations_title = models.CharField(max_length=120, help_text="max 120 characters", null=True, blank=True)
+    recommendations_subtitle = models.CharField(max_length=255, help_text="max 255 characters", null=True, blank=True)
+    recommendations_slogan = models.CharField(max_length=120, help_text="max 120 characters, belong SEE MORE button",
+                                              null=True, blank=True)
+    # FAQ block
     faq_title = models.CharField(max_length=120, help_text="max 120 characters", null=True, blank=True)
     faq_subtitle = models.CharField(max_length=120, help_text="max 120 characters", null=True, blank=True)
 
