21b388db02274f7025a5474904adce3ea60fa117
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-02-08 11:22:29 +0200

    save drafts between attempts

--- purchases/models.py
+++ purchases/models.py
@@ -5,9 +5,9 @@ from products.models import Product
 
 
 class Purchase(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
-    stripe_checkout_session_id = models.CharField(max_length=220, null=True, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.SET_NULL, null=True)
+    product = models.ManyToManyField(Product, blank=True)
+    stripe_payment_id = models.CharField(max_length=220, null=True, blank=True)
     completed = models.BooleanField(default=False)
     stripe_price = models.IntegerField(default=0)
     timestamp = models.DateTimeField(auto_now_add=True)
