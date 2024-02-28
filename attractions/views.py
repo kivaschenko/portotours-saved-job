ce6e4df0e9472744c076351d7301ea13e08a0227
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-02-28 14:44:22 +0200

    Updated attraction admin and  detail page according new FAQs logic as well

--- attractions/views.py
+++ attractions/views.py
@@ -34,10 +34,4 @@ class AttractionDetailView(DetailView):
                 lang = brother.language.code.lower()
                 url = brother.localized_url
                 self.extra_context['languages'].update({lang: url})
-        # find FAQ Destination for current language
-        current_faqs = obj.parent_attraction.faq_attractions.filter(language=obj.language).all()
-        if current_faqs:
-            self.extra_context.update({'current_faqs': current_faqs})
-        else:
-            self.extra_context['current_faqs'] = []
         return obj
