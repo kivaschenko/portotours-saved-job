684164993dcba81654eb06b86f353b5aaac5db06
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-02-29 16:16:50 +0200

    created Profile model

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from accounts.models import *  # noqa
+from accounts.models import User, Profile
 
 
 @admin.register(User)
@@ -36,4 +36,32 @@ class CustomUserAdmin(UserAdmin):
     list_display = ("email", "first_name", "last_name", "is_staff")
     list_filter = ("is_staff", "is_superuser", "is_active", "groups")
     search_fields = ("first_name", "last_name", "email")
-    
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    read_only = [
+        'shipping_address_city',
+        'shipping_address_country',
+        'shipping_address_line1',
+        'shipping_address_line2',
+        'shipping_address_postal_code',
+        'shipping_address_state',
+        'shipping_address_email',
+        'shipping_phone',
+        'shipping_name',
+    ]
+    list_display = (
+        'user',
+        'stripe_customer_id',
+        'name',
+        'phone',
+        'address_city',
+        'address_country',
+        'address_line1',
+        'address_line2',
+        'address_postal_code',
+        'address_state',
+    )
+    search_fields = ('name', 'stripe_customer_id', 'email')
+    list_per_page = 10
--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser, UserManager
 from django.utils.translation import gettext_lazy as _
 
 
+# CUSTOM USER
 class CustomUserManager(UserManager):
     def _create_user(self, email, password, **extra_fields):
         """
@@ -61,3 +62,41 @@ class User(AbstractUser):
 
     def __repr__(self):
         return f"<User(id={self.id} email={self.email} first_name={self.first_name} last_name={self.last_name}...)>"
+
+
+# PROFILE
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    stripe_customer_id = models.CharField(max_length=220, blank=True, null=True)
+    name = models.CharField(_("name"), max_length=120, blank=True, null=True, help_text="Card name")
+    email = models.EmailField(unique=True, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    # address
+    address_city = models.CharField(max_length=160, blank=True, null=True)
+    address_country = models.CharField(max_length=2, blank=True, null=True)
+    address_line1 = models.CharField(max_length=160, blank=True, null=True)
+    address_line2 = models.CharField(max_length=160, blank=True, null=True)
+    address_postal_code = models.CharField(max_length=10, blank=True, null=True)
+    address_state = models.CharField(max_length=60, blank=True, null=True)
+    # shipping address
+    shipping_address_city = models.CharField(max_length=160, blank=True, null=True)
+    shipping_address_country = models.CharField(max_length=2, blank=True, null=True)
+    shipping_address_line1 = models.CharField(max_length=160, blank=True, null=True)
+    shipping_address_line2 = models.CharField(max_length=160, blank=True, null=True)
+    shipping_address_postal_code = models.CharField(max_length=10, blank=True, null=True)
+    shipping_address_state = models.CharField(max_length=60, blank=True, null=True)
+    shipping_address_email = models.EmailField(unique=True, blank=True, null=True)
+    shipping_phone = models.CharField(max_length=20, blank=True, null=True)
+    shipping_name = models.CharField(max_length=160, blank=True, null=True)
+
+    # local
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<Profile: {self.id} | {self.name}>"
