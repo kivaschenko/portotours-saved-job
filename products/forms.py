98a7f608e8562627f82a004687924cbea2a02b84
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-03-19 16:24:19 +0200

    save changes between

--- products/forms.py
+++ products/forms.py
@@ -5,12 +5,22 @@ from schedule.models import Calendar
 from products.models import Language, ExperienceEvent
 
 
-
-
 class ExperienceEventForm(forms.ModelForm):
     class Meta:
         model = ExperienceEvent
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.calendar:
+            relation = self.instance.calendar.calendarrelation_set.first()
+            parent_experience_obj = relation.content_object
+            if parent_experience_obj.is_private:
+                self.fields.pop('max_participants')
+                self.fields.pop('special_price')
+                self.fields.pop('child_special_price')
+            else:
+                self.fields.pop('total_price')
 
 
 ExperienceEventFormSet = forms.inlineformset_factory(Calendar, ExperienceEvent, form=ExperienceEventForm)
