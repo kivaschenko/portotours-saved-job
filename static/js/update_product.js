b764de08183a18274f74bcb8ee5e745d6f3e16da
Koshliak Pasha <p.koshliak@avic.ua>
2024-03-26 14:14:16 +0200

    edits in update booking form

--- static/js/update_product.js
+++ static/js/update_product.js
@@ -36,11 +36,26 @@ const view = {
         console.log('Start Time:', startTime); // Check the value of startTime
         const input = document.querySelector('input[type="radio"][value="' + startTime + '"]');
         console.log('Input Element:', input); // Check the input element found by the query selector
-// Enable the input if found
+        const startLanguage = productData.language
+        const languageInput = document.querySelector('input[type="radio"][value="' + startLanguage + '"]')
+        const totalPrice = productData.totalPrice
+        const totalPriceTextBlock = document.querySelector('.total-price-wrapper span')
+        totalPriceTextBlock.innerHTML = `${totalPrice}`
+
+
+        // Enable the input if found
         if (input) {
             input.checked = true;
         } else {
             console.error('Input not found for the given start time:', startTime);
+        }
+
+        // Enable starting language if found 
+
+        if (languageInput) {
+            languageInput.checked = true;
+        } else {
+            console.error('Input not found for the given start language:', startLanguage);
         }
 
 
@@ -328,7 +343,9 @@ const controller = {
 
                 // Update submit button text
                 const totalPrice = totalAdultPrice + totalChildPrice;
-                submitBtn.textContent = `€${totalPrice.toFixed(2)} add to cart`;
+                submitBtn.textContent = `Save`;
+                let totalPriceText = document.querySelector('.total-price-wrapper span')
+                totalPriceText.innerHTML = `${totalPrice}`
             } else {
                 console.error('Selected event not found.');
             }
@@ -339,9 +356,10 @@ const controller = {
         // Reset adult and child total prices to $0
         document.getElementById('adultTotalPrice').textContent = '€0';
         document.getElementById('childTotalPrice').textContent = '€0';
+        document.querySelector('.total-price-wrapper span').innerHTML = '0'
 
         // Reset submit button text
-        submitBtn.textContent = 'Add to Cart';
+        submitBtn.textContent = 'Save';
     },
 
     // Function to handle click on a date cell
@@ -657,23 +675,3 @@ document.addEventListener('DOMContentLoaded', async function () {
 
 });
 
-
-// -----------
-// MOBILE FORM
-
-// open mobile form
-const openMobileBookingFormBtn = document.querySelector('.mobile-form-btn');
-const bookingForm = document.querySelector('.aside-form');
-const htmlElement = document.documentElement;
-const goBackBtn = document.querySelector('.btn-back');
-
-openMobileBookingFormBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    bookingForm.classList.add('open-mobile');
-    htmlElement.classList.add('off-scroll');
-});
-
-goBackBtn.addEventListener('click', () => {
-    bookingForm.classList.remove('open-mobile');
-    htmlElement.classList.remove('off-scroll');
-});
