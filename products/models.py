d936eb74db63df587a0beb7617245f7fac34c066
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-03-31 19:15:47 +0300

    fixed tests

--- products/models.py
+++ products/models.py
@@ -167,7 +167,7 @@ class ParentExperience(models.Model):
     is_exclusive = models.BooleanField(default=False, help_text="If this experience is exclusive then competition will propose.")
     allowed_languages = models.ManyToManyField(Language, help_text="list of languages this experience")
     free_cancellation = models.BooleanField(default=False, help_text="Free Cancellation is allowed.", null=True)
-    happy_clients_number = models.IntegerField()
+    happy_clients_number = models.IntegerField(default=0)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
