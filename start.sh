8c2ad15110d1a39f3f322a9b3bf953060c32ef9c
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-04-23 11:07:30 +0300

    try to run on 80th port

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start Gunicorn server
-gunicorn --workers 4 --bind 0.0.0.0:80 portotours.wsgi:application &
+gunicorn --workers 4 --bind 0.0.0.0:8000 portotours.wsgi:application &
 # Wait for Django to start
 sleep 10  # Adjust the sleep time as needed to ensure Django has started
 # Start Celery worker alongside Django server
