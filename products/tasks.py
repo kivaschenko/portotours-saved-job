4f39fb7e8b12d5f241a7f3887f772c9b577bfd5b
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-04-27 12:05:59 +0300

    remove sample task

--- products/tasks.py
+++ products/tasks.py
@@ -2,12 +2,6 @@ from celery import shared_task
 
 
 @shared_task()
-def add(x=2, y=3):
-    z = x + y
-    print(f"z={z}")
-
-
-@shared_task()
 def check_expired_products():
     from service_layer.services import update_products_status_if_expired
     update_products_status_if_expired()
