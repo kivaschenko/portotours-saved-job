e3e3ce587249e5caf1932d255ab53a3358af0cfa
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-05-13 17:45:19 +0300

    setup in search destinations only related with experiences

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.core.cache import cache
 
-from destinations.models import Destination  # Assuming you have a Destination model
+from destinations.models import Destination
+from products.models import Experience
 from .models import Subscriber
 
 
@@ -20,8 +22,13 @@ class ExperienceSearchForm(forms.Form):
     def __init__(self, lang, *args, initial_data=None, **kwargs):
         super(ExperienceSearchForm, self).__init__(*args, **kwargs)
         self.fields['place'] = forms.ChoiceField(choices=(('', 'Choose place'),), required=False)
-
-        destinations = Destination.objects.filter(language__code=lang.upper()).distinct().values_list('slug', 'name')
+        destinations = cache.get('destinations_{}'.format(lang))
+        if not destinations:
+            all_active_experiences = Experience.active.all()
+            destinations = (Destination.objects.filter(experience__in=all_active_experiences).
+                            filter(language__code=lang.upper()).
+                            distinct().values_list('slug', 'name'))
+            cache.set('destinations_{}'.format(lang), destinations, timeout=3600)
         self.fields['place'].choices += [(slug, name) for slug, name in destinations]
 
         self.fields['date'].widget.attrs['class'] = 'datepicker'  # Add CSS class for custom styling
