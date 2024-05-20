652e81eb40eb79be427c9fee5fdcab0d6c6b1b77
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-05-20 14:07:48 +0300

    add additional filters

--- static/google_analytics/data_pusher.js
+++ static/google_analytics/data_pusher.js
@@ -1,6 +1,7 @@
 function trackButtonClick(url, items, clickPlace) {
     items['item_list_name'] = clickPlace;
     let itemsArray = [items];
+    console.log('items', items);
     window.dataLayer.push({ecommerce: null});  // Clear the previous ecommerce object.
     window.dataLayer.push({
         event: "view_item",
