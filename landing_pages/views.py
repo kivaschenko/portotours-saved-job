b5fad0d8bf3c5cd3ee9d9952948de8415dbcee6c
Kostiantyn Ivashchenko <kivaschenko@protonmail.com>
2024-05-21 10:46:56 +0300

    save draft for filters

--- landing_pages/views.py
+++ landing_pages/views.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+
+from django.db.models import ExpressionWrapper, F, DurationField
 from django.views.generic import DetailView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.utils import timezone
@@ -16,22 +19,27 @@ class LandingPageView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        lang = 'en'
-        if self.kwargs.get('lang'):
-            lang = self.kwargs.get('lang')
-        # activate(lang)
+        lang = self.kwargs.get('lang', 'en')
         context['current_language'] = lang
         parent_experiences = ParentExperience.objects.filter(categories=self.object.category)
-        experiences = []
-        for par_exp in parent_experiences:
-            found_experience = par_exp.child_experiences.filter(is_active=True, language__code=lang.upper()).first()
-            if found_experience:
-                experiences.append(found_experience)
-        if experiences:
-            experiences_queryset = Experience.objects.filter(pk__in=[exp.pk for exp in experiences])
+
+        experiences = parent_experiences.prefetch_related(
+            'child_experiences').filter(
+            child_experiences__is_active=True,
+            child_experiences__language__code=lang.upper()
+        ).distinct()
+
+        if experiences.exists():
+            experiences_queryset = Experience.objects.filter(
+                pk__in=[exp.pk for exp in experiences]
+            )
+
             tour_type = self.request.GET.get('tour_type', 'all')
             if tour_type in ['private', 'group']:
-                experiences_queryset = experiences_queryset.filter(parent_experience__is_private=(tour_type=='private'))
+                experiences_queryset = experiences_queryset.filter(
+                    parent_experience__is_private=(tour_type=='private')
+                )
+
             sort_by = self.request.GET.get('filter_by', 'all')
             if sort_by in ['price_low', 'price_high', 'discount', 'hot_deals']:
                 order_by_field = {
@@ -42,16 +50,41 @@ class LandingPageView(DetailView):
                 }.get(sort_by)
                 if order_by_field:
                     experiences_queryset = experiences_queryset.order_by(order_by_field)
+
             time_of_day = self.request.GET.get('time_of_day', 'all')
             if time_of_day != 'all':
-                queryset_events = ExperienceEvent.objects.filter(start__day__ge=timezone.now()).filter(remaining_participants__gte=1)
-                if time_of_day == 'morning':
-                    queryset_events = queryset_events.filter(start__hour__lt=12)
-                elif time_of_day == 'afternoon':
-                    queryset_events = queryset_events.filter(start__hour__gte=12, start__hour__lt=17)
-                elif time_of_day == 'evening':
-                    queryset_events = queryset_events.filter(start__hour__gte=17)
-                events_idx = queryset_events.values_list('id', flat=True)
+                time_filters = {
+                    'morning': {'start__hour__lt': 12},
+                    'afternoon': {'start__hour__gte': 12, 'start__hour__lte': 17},
+                    'evening': {'start__hour__lte': 17},
+                }
+                queryset_events = ExperienceEvent.objects.filter(
+                    start__gte=timezone.now(),
+                    remaining_participants__gte=1,
+                    **time_filters.get(time_of_day, {})
+                )
+                event_experience_ids = queryset_events.values_list('id', flat=True)
+                experiences_queryset = experiences_queryset.filter(id__in=event_experience_ids)
+            else:
+                queryset_events = ExperienceEvent.objects.filter(
+                    start__gte=timezone.now(),
+                    remaining_participants__gte=1,
+                )
+
+            duration = self.request.GET.get('duration', 'all')
+            if duration != 'all':
+                duration_filters = {
+                    '0-1': {'duration__lte': timedelta(hours=1)},
+                    '1-4': {'duration__gt': timedelta(hours=1), 'duration__lte': 4},
+                    '4-10': {'duration__gt': timedelta(hours=4), 'duration__lte': 10},
+                    '24-72': {'duration__gt': timedelta(hours=24), 'duration__lte': 72},
+                }
+                duration_filter = duration_filters.get(duration, {})
+                queryset_events = queryset_events.annotate(
+                    duration=ExpressionWrapper(F('end') - F('start'), output_field=DurationField())
+                ).filter(**duration_filter)
+                event_experience_ids = queryset_events.values_list('experience_id', flat=True)
+                experiences_queryset = experiences_queryset.filter(id__in=event_experience_ids)
 
             if self.object.destinations.all():
                 experiences_queryset = experiences_queryset.filter(destinations__in=self.object.destinations.all())
